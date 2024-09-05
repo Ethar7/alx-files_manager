03ae78b53beb04483db3a3f528dcf766c9614e3a
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-05 18:12:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import routes from './routes/index';
 
-const appli = express();
+const app = express();
 const port = process.env.PORT || 5000;
 
-appli.use(express.json());
-appli.use('/', routes);
+app.use(express.json());
+app.use('/', routes);
 
-appli.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
