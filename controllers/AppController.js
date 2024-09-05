7e8c40320301e9230d15f99076f22982de89e919
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-05 18:14:28 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,13 +2,13 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static async getStatus(request, res) {
-    const rediusst = await redisClient.isAlive();
+  static async getStatus(req, res) {
+    const redisStatus = await redisClient.isAlive();
     const dbStatus = await dbClient.isAlive();
-    res.status(200).json({ redis: redisSt, db: dbStatus });
+    res.status(200).json({ redis: redisStatus, db: dbStatus });
   }
 
-  static async getStats(request, res) {
+  static async getStats(req, res) {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
     res.status(200).json({ users: usersCount, files: filesCount });
