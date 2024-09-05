21d954fe8c806e037dfc982b80afb88c4715f91b
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-05 23:13:03 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
-    const data = process.env.DB_DATABASE || 'files_manager';
+    const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
 
     this.client = new MongoClient(url, { useUnifiedTopology: true });
@@ -12,7 +12,7 @@ class DBClient {
 
     this.client.connect()
       .then(() => {
-        this.db = this.client.db(data);
+        this.db = this.client.db(database);
         this.isConnected = true;
         console.log('Connected to MongoDB');
       })
