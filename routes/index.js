05c62b9d75f211d0d65a20bcf4ef491555e42694
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-05 19:42:27 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController'); // Add this line
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController'); // Add this line
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew); // Add this line
+router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect); // Add this line
+router.get('/disconnect', AuthController.getDisconnect); // Add this line
+router.get('/users/me', UsersController.getMe); // Add this line
 
 module.exports = router;
