e47c7f289803a7477ce9548bf19be3d479446923
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-06 07:41:48 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,25 +1,41 @@
-// controllers/AppController.js
-import dbClient from '../utils/db';
+// controllers/AuthController.js
+import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-class AppController {
-  static async getStatus(req, res) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    return res.status(200).json(status);
+class AuthController {
+  static async getConnect(req, res) {
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const credentials = Buffer.from(authHeader.slice(6), 'base64').toString('utf-8');
+    const [email, password] = credentials.split(':');
+
+    const user = await dbClient.users.findOne({ email, password: sha1(password) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), 86400);
+
+    return res.status(200).json({ token });
   }
 
-  static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-    const stats = {
-      users: usersCount,
-      files: filesCount,
-    };
-    return res.status(200).json(stats);
+  static async getDisconnect(req, res) {
+    const token = req.header('X-Token');
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(`auth_${token}`);
+    return res.status(204).end();
   }
 }
 
-export default AppController;
+export default AuthController;
