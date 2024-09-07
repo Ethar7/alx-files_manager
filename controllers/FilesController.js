860142311c17910c5d819cd1e22d3a8b319e768b
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 15:35:18 +0300

    Update FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
 import mime from 'mime-types';
-import { ObjectId } from 'mongodb'; // استيراد ObjectId
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -72,6 +72,36 @@ class FilesController {
 
     const newFile = await dbClient.db.collection('files').insertOne(fileDocument);
     return res.status(201).json(newFile.ops[0]);
+  }
+
+  static async getShow(req, res) {
+    const userId = await redisClient.get(`auth_${req.headers['x-token']}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(req.params.id), userId: new ObjectId(userId) });
+    if (!file) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    return res.json(file);
+  }
+
+  static async getIndex(req, res) {
+    const userId = await redisClient.get(`auth_${req.headers['x-token']}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { parentId = 0, page = 0 } = req.query;
+    const files = await dbClient.db.collection('files')
+      .find({ userId: new ObjectId(userId), parentId: parentId === '0' ? '0' : new ObjectId(parentId) })
+      .skip(page * 20)
+      .limit(20)
+      .toArray();
+
+    return res.json(files);
   }
 }
 
