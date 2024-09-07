a236462bd71b679f3c01f668c0c92761a142573e
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 08:02:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-// server.js
 import express from 'express';
-import routes from './routes'; // Corrected import path
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+export default app;
