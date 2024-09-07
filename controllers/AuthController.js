0a02836c5f8e9419eba01a089d8f5f3918c328e7
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 12:23:12 +0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -24,21 +24,20 @@ class AuthController {
     const token = uuidv4();
     const key = `auth_${token}`;
     await redisClient.set(key, user._id.toString(), 86400); // 24 hours
+    return res.status(200).json({ token });
+  }
 
-      return res.status(200).json({ token });
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    static async getDisconnect(req, res) {
-      const token = req.headers['x-token'];
-      const key = `auth_${token}`;
-
-      const userId = await redisClient.get(key);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      await redisClient.del(key);
-      return res.status(204).send();
+    await redisClient.del(key);
+    return res.status(204).send();
   }
 }
 
