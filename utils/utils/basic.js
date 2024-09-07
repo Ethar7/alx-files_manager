7f99961666fe4ebbcc8c22affe1dc763aea31acc
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 08:08:23 +0300

    Create basic.js

---
+++ utils/utils/basic.js
@@ -0,0 +1,13 @@
+import { ObjectId } from 'mongodb';
+const basicUtils = {
+  isValidId(id) {
+    try {
+      ObjectId(id);
+    } catch (err) {
+      return false;
+    }
+    return true;
+  },
+};
+
+export default basicUtils;
