3bf0ae11e8e5f6b3d194410db923509c31fbd5f2
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 10:31:02 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // routes/index.js
 import express from 'express';
-import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
