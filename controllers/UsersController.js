daad927aa7c49d41bad485476be3da905ae3ffa1
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 12:04:05 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,7 @@
 // controllers/UsersController.js
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -21,7 +22,25 @@ class UsersController {
 
     const hashedPassword = sha1(password);
     const newUser = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+
     return res.status(201).json({ id: newUser.insertedId, email });
+    }
+
+    static async getMe(req, res) {
+      const token = req.headers['x-token'];
+      const key = `auth_${token}`;
+
+      const userId = await redisClient.get(key);
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
