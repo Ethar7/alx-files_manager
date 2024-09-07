4de95718f17bf2ff1b25abd04507976986643383
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 09:39:18 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,36 +1,35 @@
+// utils/db.js
 import { MongoClient } from 'mongodb';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
-const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-
 class DBClient {
-  constructor() {
-    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-      if (!err) {
-        this.db = client.db(DB_DATABASE);
-        this.usersCollection = this.db.collection('users');
-        this.filesCollection = this.db.collection('files');
-      } else {
-        console.log(err.message);
-        this.db = false;
-      }
-    });
-  }
-  isAlive() {
-    return !!this.db;
-  }
-  async nbUsers() {
-    const numberOfUsers = this.usersCollection.countDocuments();
-    return numberOfUsers;
-  }
-  async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
-    return numberOfFiles;
-  }
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}`;
+
+        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client.connect((err) => {
+            if (err) {
+                console.error('MongoDB connection error:', err);
+            } else {
+                this.db = this.client.db(database);
+            }
+        });
+    }
+
+    isAlive() {
+        return this.client.isConnected();
+    }
+
+    async nbUsers() {
+        return this.db.collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        return this.db.collection('files').countDocuments();
+    }
 }
 
 const dbClient = new DBClient();
-
 export default dbClient;
