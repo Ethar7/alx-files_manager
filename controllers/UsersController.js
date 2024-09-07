48737d8c292e46e67a0c865da6490446fb004dd2
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 12:13:00 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -24,23 +24,23 @@ class UsersController {
     const newUser = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
 
     return res.status(201).json({ id: newUser.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
+
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    static async getMe(req, res) {
-      const token = req.headers['x-token'];
-      const key = `auth_${token}`;
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-      const userId = await redisClient.get(key);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      return res.status(200).json({ id: user._id, email: user.email });
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
