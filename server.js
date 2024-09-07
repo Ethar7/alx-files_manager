f8ef82b9a1246320ad3d47c6c9d71ef7c27738c5
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 11:20:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// server.js
 import express from 'express';
 import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json()); // للتأكد من قراءة JSON من الـ body
 app.use('/', routes);
 
 app.listen(port, () => {
