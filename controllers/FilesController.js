662d2fa09ff4966b69f030aea9c8d97fa0c32550
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-09-07 13:08:56 +0300

    Update FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,70 +2,70 @@ import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import path from 'path';
 import mime from 'mime-types';
-import dbClient from '../utils/db.js';
-import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class FilesController {
-    static async postUpload(req, res) {
-        const token = req.headers['x-token'];
-        const key = `auth_${token}`;
+  static async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    const key = `auth_${token}`;
 
-        const userId = await redisClient.get(key);
-        if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        const { name, type, parentId = 0, isPublic = false, data } = req.body;
-
-        if (!name) {
-            return res.status(400).json({ error: 'Missing name' });
-        }
-
-        if (!['folder', 'file', 'image'].includes(type)) {
-            return res.status(400).json({ error: 'Missing type' });
-        }
-
-        if (type !== 'folder' && !data) {
-            return res.status(400).json({ error: 'Missing data' });
-        }
-
-        if (parentId !== 0) {
-            const parentFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(parentId) });
-            if (!parentFile) {
-                return res.status(400).json({ error: 'Parent not found' });
-            }
-            if (parentFile.type !== 'folder') {
-                return res.status(400).json({ error: 'Parent is not a folder' });
-            }
-        }
-
-        const fileDocument = {
-            userId: new ObjectId(userId),
-            name,
-            type,
-            isPublic,
-            parentId: parentId === 0 ? '0' : new ObjectId(parentId),
-        };
-
-        if (type === 'folder') {
-            const newFile = await dbClient.db.collection('files').insertOne(fileDocument);
-            return res.status(201).json(newFile.ops[0]);
-        }
-
-        const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
-        if (!fs.existsSync(folderPath)) {
-            fs.mkdirSync(folderPath, { recursive: true });
-        }
-
-        const localPath = path.join(folderPath, uuidv4());
-        fs.writeFileSync(localPath, Buffer.from(data, 'base64'));
-
-        fileDocument.localPath = localPath;
-        fileDocument.mimeType = mime.lookup(name);
-
-        const newFile = await dbClient.db.collection('files').insertOne(fileDocument);
-        return res.status(201).json(newFile.ops[0]);
+    const userId = await redisClient.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
+
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!['folder', 'file', 'image'].includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId !== 0) {
+      const parentFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(parentId) });
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const fileDocument = {
+      userId: new ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId: parentId === 0 ? '0' : new ObjectId(parentId),
+    };
+
+    if (type === 'folder') {
+      const newFile = await dbClient.db.collection('files').insertOne(fileDocument);
+      return res.status(201).json(newFile.ops[0]);
+    }
+
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+    const localPath = path.join(folderPath, uuidv4());
+    fs.writeFileSync(localPath, Buffer.from(data, 'base64'));
+
+    fileDocument.localPath = localPath;
+    fileDocument.mimeType = mime.lookup(name);
+
+    const newFile = await dbClient.db.collection('files').insertOne(fileDocument);
+    return res.status(201).json(newFile.ops[0]);
+  }
 }
 
 export default FilesController;
